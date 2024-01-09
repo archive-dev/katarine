8d32e7547f7e66da4c2eb09551de8578253f15fc
whoisamyy <egegegg002@gmail.com>
2024-01-09 22:20:12 +0300

    todo: todo: todo

--- core/src/io/github/whoisamyy/utils/Utils.java
+++ core/src/io/github/whoisamyy/utils/Utils.java
@@ -3,6 +3,10 @@ package io.github.whoisamyy.utils;
 import java.lang.reflect.Field;
 
 public class Utils {
+    /**
+     * Pixels Per Meter.
+     */
+    public static final float PPM = 64f;
     public static void setStaticFieldValue(Class<?> clazz, String fieldName, Object fieldValue) throws NoSuchFieldException, IllegalAccessException {
         Field field = clazz.getDeclaredField(fieldName);
         field.setAccessible(true);
@@ -15,5 +19,21 @@ public class Utils {
         field.setAccessible(true);
         field.set(objectInstance, fieldValue);
         field.setAccessible(false);
+    }
+
+    public static Object getStaticFieldValue(Class<?> clazz, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        Field field = clazz.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        Object o = field.get(null);
+        field.setAccessible(false);
+        return o;
+    }
+
+    public static Object getFieldValue(Object objectInstance, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        Field field = objectInstance.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        Object o = field.get(objectInstance);
+        field.setAccessible(false);
+        return o;
     }
 }
