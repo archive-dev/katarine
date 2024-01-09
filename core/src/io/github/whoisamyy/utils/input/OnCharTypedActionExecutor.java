6c4aadc56c3a3041d2543c65bf81dd1f9bccaee9
whoisamyy <egegegg002@gmail.com>
2024-01-09 22:17:28 +0300

    removed

--- core/src/io/github/whoisamyy/utils/input/OnCharTypedActionExecutor.java
+++
@@ -1,6 +0,0 @@
-package io.github.whoisamyy.utils.input;
-
-@FunctionalInterface
-public interface OnCharTypedActionExecutor {
-    void onCharTyped(char charTyped);
-}
--- core/src/io/github/whoisamyy/utils/input/OnKeyPressedActionExecutor.java
+++
@@ -1,6 +0,0 @@
-package io.github.whoisamyy.utils.input;
-
-@FunctionalInterface
-public interface OnKeyPressedActionExecutor {
-    void onKeyDown(int keyPressed);
-}
