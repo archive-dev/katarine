7acebaa7d44783584bdaceb442f2a129e08174f2
whoisamyy <egegegg002@gmail.com>
2024-01-13 22:31:41 +0300

    safety fix

--- core/src/io/github/whoisamyy/utils/Utils.java
+++ core/src/io/github/whoisamyy/utils/Utils.java
@@ -9,31 +9,35 @@ public class Utils {
     public static final float PPM = 64f;
     public static void setStaticFieldValue(Class<?> clazz, String fieldName, Object fieldValue) throws NoSuchFieldException, IllegalAccessException {
         Field field = clazz.getDeclaredField(fieldName);
+        boolean isAccessible = field.canAccess(null);
         field.setAccessible(true);
         field.set(null, fieldValue);
-        field.setAccessible(false);
+        field.setAccessible(isAccessible);
     }
 
     public static void setFieldValue(Object objectInstance, String fieldName, Object fieldValue) throws NoSuchFieldException, IllegalAccessException {
         Field field = objectInstance.getClass().getDeclaredField(fieldName);
+        boolean isAccessible = field.canAccess(objectInstance);
         field.setAccessible(true);
         field.set(objectInstance, fieldValue);
-        field.setAccessible(false);
+        field.setAccessible(isAccessible);
     }
 
     public static Object getStaticFieldValue(Class<?> clazz, String fieldName) throws NoSuchFieldException, IllegalAccessException {
         Field field = clazz.getDeclaredField(fieldName);
+        boolean isAccessible = field.canAccess(null);
         field.setAccessible(true);
         Object o = field.get(null);
-        field.setAccessible(false);
+        field.setAccessible(isAccessible);
         return o;
     }
 
     public static Object getFieldValue(Object objectInstance, String fieldName) throws NoSuchFieldException, IllegalAccessException {
         Field field = objectInstance.getClass().getDeclaredField(fieldName);
+        boolean isAccessible = field.canAccess(objectInstance);
         field.setAccessible(true);
         Object o = field.get(objectInstance);
-        field.setAccessible(false);
+        field.setAccessible(isAccessible);
         return o;
     }
 }
