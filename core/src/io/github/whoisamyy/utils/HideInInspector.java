a36000decb3d6518504da3bfbe6b0faaaeaefba4
whoisamyy <egegegg002@gmail.com>
2024-01-30 23:47:15 +0300

    created

---
+++ core/src/io/github/whoisamyy/utils/HideInInspector.java
@@ -0,0 +1,11 @@
+package io.github.whoisamyy.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HideInInspector {
+}
