8b42e0f60b83bec958c09f645e636991c73f9c16
whoisamyy <egegegg002@gmail.com>
2024-01-30 23:48:07 +0300

    updated

--- core/src/io/github/whoisamyy/components/RigidBody2D.java
+++ core/src/io/github/whoisamyy/components/RigidBody2D.java
@@ -4,20 +4,23 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
 public class RigidBody2D extends Component {
-    Transform2D transform;
+    protected TriggerBox triggerBox;
 
     public Body body;
     public Fixture fixture;
+    private World world;
 
     public RigidBody2D(World world, BodyDef bodyDef, Shape shape, float density) {
         this.body = world.createBody(bodyDef);
         this.fixture = this.body.createFixture(shape, density);
+        this.world = world;
         shape.dispose();
     }
 
     public RigidBody2D(World world, BodyDef bodyDef, FixtureDef fixtureDef) {
         this.body = world.createBody(bodyDef);
         this.fixture = this.body.createFixture(fixtureDef);
+        this.world = world;
     }
 
     @Override
@@ -26,6 +29,12 @@ public class RigidBody2D extends Component {
             transform = gameObject.getComponent(Transform2D.class);
         } catch (NullPointerException e) {
             transform = gameObject.addComponent(new Transform2D());
+        }
+
+        try {
+            triggerBox = gameObject.getComponent(TriggerBox.class);
+        } catch (NullPointerException e) {
+            triggerBox = gameObject.addComponent(new TriggerBox(fixture.getShape()));
         }
     }
 
@@ -36,11 +45,13 @@ public class RigidBody2D extends Component {
 
     @Override
     public void die() {
-        super.die();
+        world.destroyBody(body);
     }
 
     public void setPosition(Vector2 position) {
         body.setTransform(position, 0);
         if (transform!=null) transform.setPosition(position);
     }
+
+
 }
