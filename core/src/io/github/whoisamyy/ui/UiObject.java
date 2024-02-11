f03547d07f68579080258f2aac196d2bee2def1f
whoisamyy <egegegg002@gmail.com>
2024-02-11 19:36:31 +0300

    base class for any object in ui

---
+++ core/src/io/github/whoisamyy/ui/UiObject.java
@@ -0,0 +1,41 @@
+package io.github.whoisamyy.ui;
+
+import com.badlogic.gdx.math.Vector2;
+import io.github.whoisamyy.components.Component;
+import io.github.whoisamyy.objects.GameObject;
+
+public class UiObject extends Component {
+    protected final Vector2 uiPosition = new Vector2();
+
+    protected Canvas canvas;
+
+    @Override
+    public void start() {
+        if (canvas!=null) return;
+        GameObject go = gameObject;
+        while (go.getParent()!=null) {
+            try {
+                canvas = go.getComponent(Canvas.class);
+                break;
+            } catch (NullPointerException ignored) {}
+            go = go.getParent();
+        }
+    }
+
+    @Override
+    public void update() {
+        gameObject.relativePosition.set(uiPosition);
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    public Vector2 getUiPosition() {
+        return uiPosition;
+    }
+
+    public void setCanvas(Canvas canvas) {
+        this.canvas = canvas;
+    }
+}
