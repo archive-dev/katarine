5fcb2d6197d662ccfa40177b0fac8e6a29e3d377
whoisamyy <egegegg002@gmail.com>
2024-02-12 22:36:05 +0300

    added SpriteBatch field

--- core/src/io/github/whoisamyy/components/DrawableComponent.java
+++ core/src/io/github/whoisamyy/components/DrawableComponent.java
@@ -1,6 +1,9 @@
 package io.github.whoisamyy.components;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public abstract class DrawableComponent extends Component {
+    protected SpriteBatch batch;
     public boolean show = true;
 
     protected abstract void draw();
