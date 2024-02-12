d3f84d793f4b247906411fabca949faaf6ef8ac3
whoisamyy <egegegg002@gmail.com>
2024-02-12 22:31:02 +0300

    created...

---
+++ core/src/io/github/whoisamyy/ui/Anchor.java
@@ -0,0 +1,13 @@
+package io.github.whoisamyy.ui;
+
+public enum Anchor {
+    TOP_LEFT,
+    TOP_CENTER,
+    TOP_RIGHT,
+    CENTER_LEFT,
+    CENTER,
+    CENTER_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_CENTER,
+    BOTTOM_RIGHT
+}
