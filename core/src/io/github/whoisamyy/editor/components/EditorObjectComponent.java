5cc7017b0f27478e4b5e9b2c4cbf18d159a6d153
whoisamyy <egegegg002@gmail.com>
2024-02-12 22:31:57 +0300

    changed TriggerBox to EditorObjectComponent.EditorTriggerBox

--- core/src/io/github/whoisamyy/editor/components/EditorObjectComponent.java
+++ core/src/io/github/whoisamyy/editor/components/EditorObjectComponent.java
@@ -4,7 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.utils.Null;
 import io.github.whoisamyy.components.Component;
+import io.github.whoisamyy.components.Text;
 import io.github.whoisamyy.components.TriggerBox;
 import io.github.whoisamyy.editor.Editor;
 import io.github.whoisamyy.logging.LogLevel;
@@ -17,26 +19,38 @@ import java.util.LinkedHashSet;
 
 @HideInInspector
 public class EditorObjectComponent extends Component {
+    @HideInInspector
+    @EditorObject
+    public static class EditorTriggerBox extends TriggerBox {
+        public EditorTriggerBox(PolygonShape shape) {
+            super(shape);
+        }
+    }
     public static LinkedHashSet<GameObject> selection = new LinkedHashSet<>(16);
     private static EditorCamera ec;
 
-    TriggerBox triggerBox;
+    EditorTriggerBox triggerBox;
 
     private boolean selected = false;
 
     @Override
     public void awake() {
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(0.35f, 0.35f);
+        try {
+            Text t = this.gameObject.getComponentExtender(Text.class);
+            shape.setAsBox(t.getTextWidth() / 2, t.getTextHeight() / 2);
+        } catch (NullPointerException e) {
+            shape.setAsBox(0.35f, 0.35f);
+        }
 
-        gameObject.addComponent(new TriggerBox(shape));
+        gameObject.addComponent(new EditorTriggerBox(shape));
 
         logger.setLogLevel(LogLevel.DEBUG);
     }
 
     @Override
     public void start() {
-        triggerBox = gameObject.getComponent(TriggerBox.class);
+        triggerBox = gameObject.getComponent(EditorTriggerBox.class);
         ec = Editor.getInstance().getCam().getComponent(EditorCamera.class);
     }
 
@@ -56,9 +70,6 @@ public class EditorObjectComponent extends Component {
 
         if (triggerBox.isTouched() && selected && drag!=null && Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
             triggerBox.gameObject.relativePosition.add(new Vector2(drag.getDragDelta()).scl(ec.getCamera().zoom));
-            //for (GameObject c : gameObject.getChildren()) {
-            //    c.transform.pos.add(new Vector2(drag.getDragDelta()).scl(ec.getCamera().zoom));
-            //}
         }
 
         if (isKeysPressed(Input.Keys.SHIFT_LEFT, Input.Keys.D)) {
