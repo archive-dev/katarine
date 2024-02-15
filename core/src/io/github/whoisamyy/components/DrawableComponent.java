7ed8b024b405f2fc42ae99f48ff03662e31901d0
whoisamyy <egegegg002@gmail.com>
2024-02-15 22:21:12 +0300

    not implemented yet

--- core/src/io/github/whoisamyy/components/DrawableComponent.java
+++ core/src/io/github/whoisamyy/components/DrawableComponent.java
@@ -3,6 +3,8 @@ package io.github.whoisamyy.components;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public abstract class DrawableComponent extends Component {
+    public long drawOrder = 0; // 0 is first
+
     protected SpriteBatch batch;
     public boolean show = true;
 
