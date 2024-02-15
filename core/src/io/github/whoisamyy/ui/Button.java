24fa8ebd5288606df7443a2265436feebaae322a
whoisamyy <egegegg002@gmail.com>
2024-02-15 22:21:47 +0300

    added anchors

--- core/src/io/github/whoisamyy/ui/Button.java
+++ core/src/io/github/whoisamyy/ui/Button.java
@@ -32,23 +32,40 @@ public class Button extends UiObject {
     @Override
     public void awake() {
         buttonText = new Text("fonts/Roboto-Medium.ttf", 1, Color.WHITE, 1 / Utils.PPU, Color.BLACK, true);
-        buttonText.setColor(Color.BLACK);
+        buttonText.setColor(Color.RED);
         buttonText.text = "Button";
-        if (anchor == Anchor.CENTER) {
-            buttonText.getPos().set(-buttonSize.x / 2 + textPadding.x, buttonSize.y / 2 - textPadding.y);
-        }
         PolygonShape shape = new PolygonShape();
         shape.setAsBox(buttonSize.x/2, buttonSize.y/2);
 
-        gameObject.addComponent(buttonText);
         triggerBox = gameObject.addComponent(new TriggerBox(shape));
         button = gameObject.addComponent(new Sprite(new Texture(Gdx.files.internal("whitepx.png")), 3, 1));
+        gameObject.addComponent(buttonText);
+
+        button.updateOrder = buttonText.updateOrder+1;
+    }
+
+    @Override
+    public void start() {
+        switch (anchor) {
+            case TOP_LEFT -> buttonText.getPos().sub((buttonSize.x / 2) - textPadding.x, -(buttonSize.y / 2 - textPadding.y));
+            case CENTER_LEFT -> buttonText.getPos().sub((buttonSize.x / 2) - textPadding.x, -buttonText.getTextHeight()/2);
+            case BOTTOM_LEFT -> buttonText.getPos().sub((buttonSize.x / 2) - textPadding.x, -textPadding.y);
+
+            case CENTER -> buttonText.getPos().set(triggerBox.getBody().getWorldCenter().cpy().sub(buttonText.getTextWidth() / 2, -buttonText.getTextHeight() / 2));
+            case TOP_CENTER -> buttonText.getPos().add(-buttonText.getTextWidth()/2, buttonSize.y / 2 - textPadding.y);
+            case BOTTOM_CENTER -> buttonText.getPos().sub(buttonText.getTextWidth()/2, -textPadding.y);
+
+            case TOP_RIGHT -> buttonText.getPos().add((buttonSize.x / 2) - buttonText.getTextWidth() - textPadding.x, buttonSize.y / 2 - textPadding.y);
+            case CENTER_RIGHT -> buttonText.getPos().add((buttonSize.x / 2) - buttonText.getTextWidth() - textPadding.x, buttonText.getTextHeight()/2);
+            case BOTTOM_RIGHT -> buttonText.getPos().add((buttonSize.x / 2) - buttonText.getTextWidth() - textPadding.x, textPadding.y);
+        }
     }
 
     @Override
     public void update() {
         super.update();
-//        if (Editor.getInstance()!=null || Game.getInstance().isEditorMode()) return;
+
+        if (Editor.getInstance()!=null || Game.getInstance().isEditorMode()) return;
         if (triggerBox.isTouched() && isButtonPressed(Input.Buttons.LEFT)) {
             button.getSprites().forEach(s -> {
                 s.setColor(secondaryColor);
