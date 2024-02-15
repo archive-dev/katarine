bc3faa3c2bf4dc5479c6792d881a2ccd90e9fca5
whoisamyy <egegegg002@gmail.com>
2024-02-15 22:21:02 +0300

    now works fine with text

--- core/src/io/github/whoisamyy/editor/components/EditorObjectComponent.java
+++ core/src/io/github/whoisamyy/editor/components/EditorObjectComponent.java
@@ -3,14 +3,17 @@ package io.github.whoisamyy.editor.components;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.utils.Null;
+import com.badlogic.gdx.utils.Array;
 import io.github.whoisamyy.components.Component;
 import io.github.whoisamyy.components.Text;
 import io.github.whoisamyy.components.TriggerBox;
 import io.github.whoisamyy.editor.Editor;
 import io.github.whoisamyy.logging.LogLevel;
 import io.github.whoisamyy.objects.GameObject;
+import io.github.whoisamyy.ui.Button;
 import io.github.whoisamyy.utils.EditorObject;
 import io.github.whoisamyy.utils.HideInInspector;
 import io.github.whoisamyy.utils.input.MouseClickEvent;
@@ -22,8 +25,35 @@ public class EditorObjectComponent extends Component {
     @HideInInspector
     @EditorObject
     public static class EditorTriggerBox extends TriggerBox {
+        public Vector2 relativePos = new Vector2();
+
         public EditorTriggerBox(PolygonShape shape) {
             super(shape);
+        }
+
+        @Override
+        public void update() {
+            this.body.setTransform(transform.pos.cpy().add(relativePos), 0);
+
+            Array<Contact> contacts = world.getContactList();
+
+            isTouched = false;
+
+            for (Contact contact : contacts) {
+                if (contact.getFixtureA()==this.fixture && contact.getFixtureB()==Editor.getInstance().getCursorBox().getFixture()) {
+                    isTouched = true;
+                    break;
+                }
+                if (contact.getFixtureB()==this.fixture && contact.getFixtureA()==Editor.getInstance().getCursorBox().getFixture()) {
+                    isTouched = true;
+                    break;
+                }
+                if (contact.getFixtureA().getBody().getType() == BodyDef.BodyType.DynamicBody && contact.getFixtureB().getBody().getType() == BodyDef.BodyType.DynamicBody ||
+                        contact.getFixtureB().getBody().getType() == BodyDef.BodyType.DynamicBody && contact.getFixtureA().getBody().getType() == BodyDef.BodyType.DynamicBody) {
+                    isTouched = Editor.getInstance()!=null;
+                    break;
+                }
+            }
         }
     }
     public static LinkedHashSet<GameObject> selection = new LinkedHashSet<>(16);
@@ -36,12 +66,7 @@ public class EditorObjectComponent extends Component {
     @Override
     public void awake() {
         PolygonShape shape = new PolygonShape();
-        try {
-            Text t = this.gameObject.getComponentExtender(Text.class);
-            shape.setAsBox(t.getTextWidth() / 2, t.getTextHeight() / 2);
-        } catch (NullPointerException e) {
-            shape.setAsBox(0.35f, 0.35f);
-        }
+        shape.setAsBox(0.35f, 0.35f);
 
         gameObject.addComponent(new EditorTriggerBox(shape));
 
@@ -51,6 +76,16 @@ public class EditorObjectComponent extends Component {
     @Override
     public void start() {
         triggerBox = gameObject.getComponent(EditorTriggerBox.class);
+        try {
+            gameObject.getComponentExtender(Button.class);
+        } catch (NullPointerException e) {
+            try {
+                Text t = this.gameObject.getComponentExtender(Text.class);
+                triggerBox.relativePos.set(t.getTextWidth() / 2, -t.getTextHeight() / 2);
+                triggerBox.setShapeAsBox(t.getTextWidth() / 2, t.getTextHeight() / 2);
+            } catch (NullPointerException ignored) {}
+        }
+
         ec = Editor.getInstance().getCam().getComponent(EditorCamera.class);
     }
 
