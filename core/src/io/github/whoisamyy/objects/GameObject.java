734575f259e6c7b747c3eb557c9db2552de914c8
whoisamyy <egegegg002@gmail.com>
2024-02-23 16:14:02 +0300

    fixed + reworked + improved

--- core/src/io/github/whoisamyy/objects/GameObject.java
+++ core/src/io/github/whoisamyy/objects/GameObject.java
@@ -1,16 +1,18 @@
 package io.github.whoisamyy.objects;
 
 import com.badlogic.gdx.math.Vector2;
+import com.sun.jdi.connect.Connector;
 import io.github.whoisamyy.components.Component;
 import io.github.whoisamyy.components.RigidBody2D;
 import io.github.whoisamyy.components.Transform2D;
 import io.github.whoisamyy.editor.Editor;
 import io.github.whoisamyy.editor.components.EditorObjectComponent;
 import io.github.whoisamyy.katarine.Game;
-import io.github.whoisamyy.logging.Logger;
 import io.github.whoisamyy.katarine.annotations.EditorObject;
 import io.github.whoisamyy.katarine.annotations.NotInstantiatable;
+import io.github.whoisamyy.logging.Logger;
 import io.github.whoisamyy.utils.input.AbstractInputHandler;
+import io.github.whoisamyy.utils.structs.UniqueClassPriorityQueue;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
@@ -19,29 +21,29 @@ import java.util.*;
 @EditorObject
 public class GameObject extends AbstractInputHandler {
     // game objects queued to be added in editor/game objects list
-    public static ArrayDeque<GameObject> creationQueue = new ArrayDeque<>(32);
-    public static ArrayDeque<GameObject> destroyQueue = new ArrayDeque<>(32);
+//    public static ArrayDeque<GameObject> Editor.gameObjectsCreationQueue = new ArrayDeque<>(32);
+//    public static ArrayDeque<GameObject> destroyQueue = new ArrayDeque<>(32);
     private static Editor game;
     private static Editor editor;
     private static long lastId = 1;
     private String name = toString();
     private long id;
-    protected PriorityQueue<Component> components = new PriorityQueue<>(new ComponentComparator());
-    private class ComponentComparator implements Comparator<Component> {
+    protected final UniqueClassPriorityQueue<Component> components = new UniqueClassPriorityQueue<>(new ComponentComparator());
+    private static class ComponentComparator implements Comparator<Component> {
         @Override
         public int compare(Component o1, Component o2) {
             return Integer.compare(o1.updateOrder, o2.updateOrder);
         }
     }
 
-    private HashSet<Class<? extends Component>> componentsClasses = new HashSet<>();
+    private final HashSet<Class<? extends Component>> componentsClasses = new HashSet<>();
     protected HashSet<GameObject> children = new HashSet<>();
     protected GameObject parent;
     private boolean initialized = false;
     public Transform2D transform;
     public int updateOrder = 0; // where 0 is first
 
-    public Vector2 relativePosition = new Vector2();
+    public final Vector2 relativePosition = new Vector2();
 
     private static Logger logger = new Logger(GameObject.class.getTypeName());
 
@@ -73,7 +75,7 @@ public class GameObject extends AbstractInputHandler {
             } catch (NullPointerException ignored) {}
             // Game.getInstance().getGameObjects().remove(this);
         }
-        destroyQueue.addLast(this);
+        Editor.gameObjectsDestroyQueue.addLast(this);
     }
 
     /**
@@ -96,18 +98,18 @@ public class GameObject extends AbstractInputHandler {
 
         try {
             T ret = gameObjectClass.getDeclaredConstructor().newInstance();
+
+            if (caller.equals(Editor.class) || Editor.instance!=null) {
+                EditorObjectComponent eoc = new EditorObjectComponent();
+                eoc.gameObject = ret;
+                ret.components.add(eoc);
+            }
+
             ret.setId(lastId);
             lastId++;
             ret.init();
 
-            if (caller.equals(Editor.class)) {
-                ret.addComponent(new EditorObjectComponent());
-            } else {
-                if (Editor.instance != null) {
-                    ret.addComponent(new EditorObjectComponent());
-                }
-            }
-            creationQueue.addLast(ret);
+            Editor.gameObjectsCreationQueue.addLast(ret);
 
             return ret;
         } catch (NoSuchMethodException e) {
@@ -147,18 +149,18 @@ public class GameObject extends AbstractInputHandler {
 
         try {
             T ret = gameObjectClass.getDeclaredConstructor(paramsTypes).newInstance(constructorParams);
+
+            if (caller.equals(Editor.class) || Editor.instance!=null) {
+                EditorObjectComponent eoc = new EditorObjectComponent();
+                eoc.gameObject = ret;
+                ret.components.add(eoc);
+            }
+
             ret.setId(lastId);
             lastId++;
             ret.init();
 
-            if (caller.equals(Editor.class)) {
-                ret.addComponent(new EditorObjectComponent());
-            } else {
-                if (Editor.instance != null) {
-                    ret.addComponent(new EditorObjectComponent());
-                }
-            }
-            creationQueue.addLast(ret);
+            Editor.gameObjectsCreationQueue.addLast(ret);
 
             return ret;
         } catch (NoSuchMethodException e) {
@@ -209,18 +211,17 @@ public class GameObject extends AbstractInputHandler {
                 throw new RuntimeException("Cannot instantiate " + gameObject.getClass() + " because the class is marked as not instantiatable");
         }
 
+        if (caller.equals(Editor.class) || Editor.instance!=null) {
+            EditorObjectComponent eoc = new EditorObjectComponent();
+            eoc.gameObject = gameObject;
+            gameObject.components.add(eoc);
+        }
+
         gameObject.setId(lastId);
         lastId++;
         gameObject.init();
 
-        if (caller.equals(Editor.class)) {
-            gameObject.addComponent(new EditorObjectComponent());
-        } else {
-            if (Editor.instance != null) {
-                gameObject.addComponent(new EditorObjectComponent());
-            }
-        }
-        creationQueue.addLast(gameObject);
+        Editor.gameObjectsCreationQueue.addLast(gameObject);
 
         return gameObject;
     }
@@ -255,11 +256,11 @@ public class GameObject extends AbstractInputHandler {
         if (!initialized) throw new IllegalStateException("Cannot create uninitialized GameObject");
         start();
         for (Component c : components) {
-            c.start();
+            c.create();
         }
         logger.debug("Created gameObject "+this);
         if (this.parent!=null) {
-            relativePosition = parent.transform.pos.cpy().sub(this.transform.pos);
+            relativePosition.set(parent.transform.pos.cpy().sub(this.transform.pos));
         } else {
             this.relativePosition.set(transform.pos);
         }
@@ -281,7 +282,7 @@ public class GameObject extends AbstractInputHandler {
     }
 
     /**
-     * calls {@link GameObject#die()} on this object and its components, and then actually "dies". That means this object will not be updated or rendered by Game class, and will not be accessible anymore.
+     * calls {@link GameObject#die()} on this object and {@link Component#die()} on all of its components, and then actually "dies". That means this object will not be updated or rendered by Game class, and will not be accessible anymore.
      */
     public void dispose() {
         die();
@@ -293,39 +294,47 @@ public class GameObject extends AbstractInputHandler {
         child.parent = this;
     }
 
+    /**
+     * Adds a component to the game object and initializes component ({@link Component#init()}).
+     * @param component component to be added
+     * @return added component
+     * @param <T>
+     */
     @SuppressWarnings("unchecked")
     public final <T extends Component> T addComponent(T component) {
-        if (component.gameObject!=null) throw new RuntimeException("Cannot add component "+component.getClass().getName()+" to "+this.getClass().getName()+", because it is already added to "+component.gameObject.getClass().getName());
-        if (components.contains(component) || componentsClasses.contains(component.getClass())) return (T) getComponent(component.getClass());
-        components.add(component);
+        if (component.gameObject!=null) throw new RuntimeException("Cannot add component "+component.getClass().getName()+" to "+this.getClass().getName()+", because it is already attached to "+component.gameObject.getClass().getName());
+        if (components.contains(component)) return (T) getComponent(component.getClass());
+        if (componentsClasses.contains(component.getClass()))
+            for (Component c : Editor.componentsCreationQueue)
+                if (c.getClass() == component.getClass()) return (T) c;
+
+        Editor.componentsCreationQueue.addLast(component);
         component.setGameObject(this);
         component.gameObject=this;
         componentsClasses.add(component.getClass());
-        if (!component.isInitialized())
-            component.init();
+        component.init();
         logger.debug("added component "+component.getClass().getName() + " to "+ component.gameObject.getClass().getName());
         // components.stream().sorted((o1, o2) -> Integer.compare(o1.updateOrder, o2.updateOrder)).collect(Collectors.toList()); // ill try to ADD elements in orderm don't think it's too time complex
         return component;
     }
 
-    public final boolean removeComponent(Component component) {
-        return removeComponent(component.getClass());
+    public final void removeComponent(Component component) {
+        removeComponent(component.getClass());
     }
 
     /**
      * Removes component from this object
-     * @param componentClass
-     * @return true if component was removed from this object and false otherwise
+     * @param componentClass component class
      */
-    public final boolean removeComponent(Class<? extends Component> componentClass) {
-        for (Iterator<Component> itr = components.iterator(); itr.hasNext();) {
-            if (itr.next().getClass()==componentClass) {
-                itr.remove();
-                componentsClasses.remove(componentClass);
-                return true;
+    public final void removeComponent(Class<? extends Component> componentClass) {
+        for (Component c : components) {
+            if (c.getClass()==componentClass) {
+                components.remove(c);
+                componentsClasses.remove(c.getClass());
+                c.gameObject = null;
+                break;
             }
         }
-        return false;
     }
 
     @SuppressWarnings("unchecked")
@@ -357,6 +366,12 @@ public class GameObject extends AbstractInputHandler {
                 return (T) c;
             }
         }
+        for (Component c : Editor.componentsCreationQueue) {
+            if (c.getClass() == componentClass && c.gameObject == this) {
+                return (T) c;
+            }
+        }
+
         throw new NullPointerException(this.getClass().getName() + " does not have "+componentClass.getName()+ " component");
     }
 
@@ -372,15 +387,15 @@ public class GameObject extends AbstractInputHandler {
         return result;
     }
 
-    public final PriorityQueue<Component> getComponents() {
+    public final UniqueClassPriorityQueue<Component> getComponents() {
         return components;
     }
 
-    void setId(long id) {
+    final void setId(long id) {
         this.id = id;
     }
 
-    public long getId() {
+    public final long getId() {
         return id;
     }
 
@@ -388,19 +403,19 @@ public class GameObject extends AbstractInputHandler {
         return lastId;
     }
 
-    public String getName() {
+    public final String getName() {
         return name;
     }
 
-    public HashSet<GameObject> getChildren() {
+    public final HashSet<GameObject> getChildren() {
         return children;
     }
 
-    public GameObject getParent() {
+    public final GameObject getParent() {
         return parent;
     }
 
-    public Vector2 getRelativePosition() {
+    public final Vector2 getRelativePosition() {
         return relativePosition;
     }
 
@@ -408,11 +423,11 @@ public class GameObject extends AbstractInputHandler {
         this.name = name;
     }
 
-    public int getUpdateOrder() {
+    public final int getUpdateOrder() {
         return updateOrder;
     }
 
-    public void setUpdateOrder(int updateOrder) {
+    public final void setUpdateOrder(int updateOrder) {
         this.updateOrder = updateOrder;
     }
 }
