fd04977510d2f9ab8bc7e36b601e7ca5cee7200b
whoisamyy <egegegg002@gmail.com>
2024-04-21 00:02:21 +0300

    refactor

--- core/src/io/github/whoisamyy/editor/components/CursorHandler.java
+++ core/src/io/github/whoisamyy/editor/components/CursorHandler.java
@@ -2,75 +2,91 @@ package io.github.whoisamyy.editor.components;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Cursor;
+import imgui.flag.ImGuiMouseCursor;
 import io.github.whoisamyy.components.Component;
 import io.github.whoisamyy.katarine.annotations.EditorObject;
 import io.github.whoisamyy.katarine.annotations.NotInstantiatable;
 import io.github.whoisamyy.objects.GameObject;
+import io.github.whoisamyy.ui.imgui.ImGui;
 import io.github.whoisamyy.utils.input.AbstractInputHandler;
 import io.github.whoisamyy.utils.input.MouseClickEvent;
+import org.lwjgl.glfw.GLFW;
 
 @ForbidSelection
 @EditorObject
 @NotInstantiatable
 public final class CursorHandler extends Component {
-    public static CursorHandler instance;
+    public static final CursorHandler instance = new CursorHandler();
 
-    private final Cursor.SystemCursor CURSOR_MOVE_NS = Cursor.SystemCursor.VerticalResize;
-    private final Cursor.SystemCursor CURSOR_MOVE_WE = Cursor.SystemCursor.HorizontalResize;
-    private final Cursor.SystemCursor CURSOR_MOVE_WENS = Cursor.SystemCursor.AllResize;
-    private final Cursor.SystemCursor DEFAULT = Cursor.SystemCursor.Arrow;
-    private final Cursor.SystemCursor HAND_MOVE = Cursor.SystemCursor.Hand;
+    public enum Cursors {
+        CURSOR_MOVE_NS(Cursor.SystemCursor.VerticalResize, GLFW.GLFW_VRESIZE_CURSOR, ImGuiMouseCursor.ResizeNS),
+        CURSOR_MOVE_WE(Cursor.SystemCursor.HorizontalResize, GLFW.GLFW_HRESIZE_CURSOR, ImGuiMouseCursor.ResizeEW),
+        CURSOR_MOVE_WENS(Cursor.SystemCursor.AllResize, GLFW.GLFW_RESIZE_ALL_CURSOR, ImGuiMouseCursor.ResizeAll),
+        DEFAULT(Cursor.SystemCursor.Arrow, GLFW.GLFW_ARROW_CURSOR, ImGuiMouseCursor.Arrow),
+        HAND_MOVE(Cursor.SystemCursor.Hand, GLFW.GLFW_HAND_CURSOR, ImGuiMouseCursor.Hand),
+        RESIZE_NESW(Cursor.SystemCursor.NESWResize, GLFW.GLFW_RESIZE_NESW_CURSOR, ImGuiMouseCursor.ResizeNESW),
+        RESIZE_NWSE(Cursor.SystemCursor.NWSEResize, GLFW.GLFW_RESIZE_NWSE_CURSOR, ImGuiMouseCursor.ResizeNWSE);
+
+        public final Cursor.SystemCursor gdxCursor;
+        public final int glfwCursor;
+        public final int imguiCursor;
+
+        Cursors(Cursor.SystemCursor gdxCursor, int glfwCursor, int imguiCursor) {
+            this.gdxCursor = gdxCursor;
+            this.glfwCursor = glfwCursor;
+            this.imguiCursor = imguiCursor;
+        }
+    }
+
+    public Cursors currentCursor = Cursors.DEFAULT;
 
     public static CursorHandler instance() {
-        if (instance == null) {
-            instance = new CursorHandler();
-        }
         return instance;
     }
 
-    private CursorHandler() {
-//        this.CURSOR_MOVE_NS =   Cursor.SystemCursor.VerticalResize;     //Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("cursor_move_ns.png")), 0, 0);
-//        this.CURSOR_MOVE_WE =   Cursor.SystemCursor.HorizontalResize;   //Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("cursor_move_we.png")), 0, 0);
-//        this.CURSOR_MOVE_WENS = Cursor.SystemCursor.AllResize;          //Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("cursor_move_wens.png")), 0, 0);
-    }
+    private CursorHandler() {}
 
     @Override
     public void update() {
         MouseClickEvent mce = AbstractInputHandler.getMoveEvent();
-        if (mce == null) {
-            mce = AbstractInputHandler.getDragEvent();
-        }
 
         boolean movingObject = false;
-        for (GameObject s : EditorObjectComponent.selection) {
-            movingObject = movingObject || (s.getComponent(EditorObjectComponent.class).isSelected() &&
-                    !s.getComponent(EditorObjectComponent.class).rect.isPointOnEdge(mce.getMousePosition()) &&
-                    Gdx.input.isButtonPressed(com.badlogic.gdx.Input.Buttons.LEFT));
-        }
-
         boolean onEdge = false;
         for (GameObject s : EditorObjectComponent.selection) {
-            if (s.getComponent(EditorObjectComponent.class).rect.isPointOnEdge(mce.getMousePosition()) && !movingObject && s.getComponent(EditorObjectComponent.class).isResizable()) {
+
+            if (s.getComponent(EditorObjectComponent.class).rect.isPointOnEdge(mce.getMousePosition()) &&
+                    s.getComponent(EditorObjectComponent.class).isResizable()) {
+                // if mouse is on edge of any game object, not moving any object and the object is resizable
+                // then change cursor type
+
                 onEdge = true;
                 EditorObjectComponent eoc = s.getComponent(EditorObjectComponent.class);
                 int edge = eoc.rect.getEdgeOfPoint(mce.getMousePosition());
                 if (edge == 0 || edge == 2) {
-                    Gdx.graphics.setSystemCursor(CURSOR_MOVE_WE);
+                    currentCursor = Cursors.CURSOR_MOVE_WE;
                 } else if (edge == 1 || edge == 3) {
-                    Gdx.graphics.setSystemCursor(CURSOR_MOVE_NS);
+                    currentCursor = Cursors.CURSOR_MOVE_NS;
                 } else if (edge == 4 || edge == 6) {
-                    Gdx.graphics.setSystemCursor(Cursor.SystemCursor.NESWResize);
+                    currentCursor = Cursors.RESIZE_NESW;
                 } else if (edge == 5 || edge == 7) {
-                    Gdx.graphics.setSystemCursor(Cursor.SystemCursor.NWSEResize);
+                    currentCursor = Cursors.RESIZE_NWSE;
                 }
+                break;
+            }
+
+            if (!s.getComponent(EditorObjectComponent.class).rect.isPointOnEdge(mce.getMousePosition()) &&
+                    s.getComponent(EditorObjectComponent.class).moving) {
+                movingObject = true;
+                currentCursor = Cursors.CURSOR_MOVE_WENS;
                 break;
             }
         }
 
-        if (movingObject) Gdx.graphics.setSystemCursor(CURSOR_MOVE_WENS);
-
         if (!onEdge && !movingObject) {
-            Gdx.graphics.setSystemCursor(DEFAULT);
+            currentCursor = Cursors.DEFAULT;
         }
+
+        if (!ImGui.controlsInput)
+            Gdx.graphics.setSystemCursor(currentCursor.gdxCursor);
     }
 }
