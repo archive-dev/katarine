076c243521b563e67b1b1e5f2d057984e6b9b258
whoisamyy <egegegg002@gmail.com>
2024-04-21 22:21:49 +0300

    added window flags and fixed window going outside screen

--- core/src/io/github/whoisamyy/ui/imgui/Panel.java
+++ core/src/io/github/whoisamyy/ui/imgui/Panel.java
@@ -1,6 +1,8 @@
 package io.github.whoisamyy.ui.imgui;
 
+import com.badlogic.gdx.Gdx;
 import imgui.ImGui;
+import imgui.flag.ImGuiWindowFlags;
 
 /**
  * Acts as wrapper for everything between {@link imgui.ImGui#begin(String)} and {@link imgui.ImGui#end()}
@@ -8,6 +10,10 @@ import imgui.ImGui;
 public final class Panel extends ImGuiObject {
     private String name;
     private Gui gui;
+    /**
+     * ImGuiWindowFlags
+     */
+    private int flags = ImGuiWindowFlags.None;
 
     public Panel(String name) {
         this.name = name;
@@ -18,12 +24,34 @@ public final class Panel extends ImGuiObject {
         this.gui = gui;
     }
 
+    public Panel(String name, int flags) {
+        this.name = name;
+        this.flags = flags;
+    }
+
+    public Panel(String name, Gui gui, int flags) {
+        this.name = name;
+        this.gui = gui;
+        this.flags = flags;
+    }
+
     public void setGui(Gui gui) {
         this.gui = gui;
     }
 
     public void render() {
-        ImGui.begin(name);
+        ImGui.begin(name, flags);
+
+        if (ImGui.getWindowPosX()<0) ImGui.setWindowPos(0, ImGui.getWindowPosY());
+
+        if (ImGui.getWindowPosX()>Gdx.graphics.getWidth()-ImGui.getWindowSizeX()) ImGui.setWindowPos(Gdx.graphics.getWidth()-ImGui.getWindowSizeX(),
+                ImGui.getWindowPosY());
+
+        if (ImGui.getWindowPosY()<0) ImGui.setWindowPos(ImGui.getWindowPosX(), 0);
+
+        if (ImGui.getWindowPosY()>Gdx.graphics.getHeight()-ImGui.getWindowSizeY()) ImGui.setWindowPos(ImGui.getWindowPosX(),
+                Gdx.graphics.getHeight()-ImGui.getWindowSizeY());
+
         gui.render();
         ImGui.end();
     }
