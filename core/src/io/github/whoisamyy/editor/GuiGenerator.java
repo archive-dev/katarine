c8bf697532866cbd669b96136ec46fe455b41636
whoisamyy <egegegg002@gmail.com>
2024-05-01 00:16:27 +0300

    added generators for game objects

--- core/src/io/github/whoisamyy/editor/GuiGenerator.java
+++ core/src/io/github/whoisamyy/editor/GuiGenerator.java
@@ -1,18 +1,25 @@
 package io.github.whoisamyy.editor;
 
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 import imgui.ImGui;
+import imgui.flag.ImGuiMouseButton;
+import imgui.flag.ImGuiTreeNodeFlags;
 import imgui.type.ImString;
 import io.github.whoisamyy.components.Component;
 import io.github.whoisamyy.components.Transform2D;
+import io.github.whoisamyy.editor.components.EditorObjectComponent;
 import io.github.whoisamyy.objects.GameObject;
 import io.github.whoisamyy.ui.imgui.AppendableGui;
 import io.github.whoisamyy.ui.imgui.GuiBuilder;
 import io.github.whoisamyy.utils.serialization.annotations.HideInInspector;
 import io.github.whoisamyy.utils.serialization.annotations.Range;
 import io.github.whoisamyy.utils.serialization.annotations.SerializeField;
+import io.github.whoisamyy.utils.structs.tree.Node;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
 
 public class GuiGenerator {
     public GuiGenerator() {
@@ -24,6 +31,8 @@ public class GuiGenerator {
 
         for (var f : fields) {
             if (f.isAnnotationPresent(HideInInspector.class)) continue;
+            if (Modifier.isFinal(f.getModifiers()) && !Collection.class.isAssignableFrom(f.getType())
+                && !Vector.class.isAssignableFrom(f.getType())) continue;
             boolean canAccess;
             try {
                 canAccess = f.canAccess(component);
@@ -101,8 +110,18 @@ public class GuiGenerator {
                 gui.add(() -> {
                     try {
                         Class<?> type = f.getType();
-
-                    } catch (Exception e) {}
+                        if (type.isEnum()) {
+                            if (ImGui.beginCombo(f.getName(), ((Enum<?>) f.get(component)).name())) {
+                                for (var e : type.getEnumConstants()) {
+                                    if (ImGui.selectable(e.toString())) {
+                                        f.set(component, e);
+//                                        ImGui.endCombo();
+                                    }
+                                }
+                                ImGui.endCombo();
+                            }
+                        }
+                    } catch (Exception ignored) {}
                 });
             }
             //TODO: add more types
@@ -125,6 +144,7 @@ public class GuiGenerator {
            }
 
            for (var c : obj.getComponents()) {
+               if (c.getClass().isAnnotationPresent(HideInInspector.class)) continue;
                if (ImGui.treeNode(c.getClass().getSimpleName())) {
                    generate(c).render();
                    ImGui.treePop();
@@ -149,5 +169,88 @@ public class GuiGenerator {
 
         return gui.get();
     }
-}
 
+    private static final int leafFlags = ImGuiTreeNodeFlags.OpenOnDoubleClick | ImGuiTreeNodeFlags.Leaf;
+    private static final int treeNodeFlags = ImGuiTreeNodeFlags.OpenOnArrow | ImGuiTreeNodeFlags.OpenOnDoubleClick
+            | ImGuiTreeNodeFlags.SpanAvailWidth;
+
+    public AppendableGui generateGameObjectsTree(Collection<GameObject> objects) {
+        final GuiBuilder gui = new GuiBuilder();
+
+        gui.add(() -> {
+            for (var o : objects) {
+                if (o.getClass().isAnnotationPresent(HideInInspector.class)) continue;
+                if (!o.getChildren().isEmpty()) {
+                    if (ImGui.treeNodeEx(o.getName(), treeNodeFlags)) {
+                        generateGameObjectTree(o.toTree()).render();
+                        ImGui.treePop();
+                    }
+                } else {
+                    if (ImGui.treeNodeEx(o.getName(), leafFlags)) {
+                        generateGameObjectTree(o.toTree()).render();
+                        ImGui.treePop();
+                    }
+                }
+            }
+        });
+
+        return gui.get();
+    }
+
+    public static GameObject selectedGameObject;
+
+    public AppendableGui generateGameObjectTree(Node<GameObject> tree) {
+        final GuiBuilder gui = new GuiBuilder();
+
+        gui.add(() -> {
+            int selected = 0;
+            try {
+                selected = tree.get().getComponent(EditorObjectComponent.class).selected ? 1 : 0;
+            } catch (NullPointerException ignored) {}
+            if (!tree.getChildren().isEmpty()) {
+                if (ImGui.treeNodeEx(tree.get().getName(), treeNodeFlags | selected)) {
+                    try {
+                        if (ImGui.isItemClicked(ImGuiMouseButton.Left)) {
+                            if (!ImGui.getIO().getKeyCtrl()) {
+                                EditorObjectComponent.selection.forEach(go ->
+                                        go.getComponent(EditorObjectComponent.class).selected = false);
+                                EditorObjectComponent.selection.clear();
+                            }
+                            boolean b;
+                            b = tree.get().getComponent(EditorObjectComponent.class).selected =
+                                    !tree.get().getComponent(EditorObjectComponent.class).selected;
+                            if (b)
+                                EditorObjectComponent.selection.add(tree.get());
+                        }
+                    } catch (NullPointerException ignored) {}
+                    for (var c : tree.getChildren()) {
+                        generateGameObjectTree(c).render();
+                    }
+
+                    ImGui.treePop();
+                }
+            } else {
+                if (ImGui.treeNodeEx(tree.get().getName(), leafFlags | selected)) {
+                    try {
+                        if (ImGui.isItemClicked(ImGuiMouseButton.Left)) {
+                            if (!ImGui.getIO().getKeyCtrl()) {
+                                EditorObjectComponent.selection.forEach(go ->
+                                        go.getComponent(EditorObjectComponent.class).selected = false);
+                                EditorObjectComponent.selection.clear();
+                            }
+                            boolean b;
+                            b = tree.get().getComponent(EditorObjectComponent.class).selected =
+                                    !tree.get().getComponent(EditorObjectComponent.class).selected;
+                            if (b)
+                                EditorObjectComponent.selection.add(tree.get());
+                        }
+                    } catch (NullPointerException ignored) {}
+
+                    ImGui.treePop();
+                }
+            }
+        });
+
+        return gui.get();
+    }
+}
