6b1737159b63d1931a1f879fd0f7e7ab79b3ba0f
whoisamyy <egegegg002@gmail.com>
2024-05-03 23:16:38 +0300

    created, read javadoc

---
+++ core/src/io/github/whoisamyy/utils/serialization/annotations/GuiSupplier.java
@@ -0,0 +1,15 @@
+package io.github.whoisamyy.utils.serialization.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Marks method as ImGui gui supplier.
+ * This annotation can be only used on non-static methods with no parameters. Otherwise {@code MethodNotFoundException} will be thrown.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GuiSupplier {}
