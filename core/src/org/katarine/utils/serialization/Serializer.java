a00b3c26a96a3f24ae422e63a1eb035d7384c990
whoisamyy <egegegg002@gmail.com>
2024-05-26 00:24:17 +0300

    moved static methods from Serializer to ObjectRepresentation
    added javadocs to some methods and some other methods

--- core/src/org/katarine/utils/serialization/Serializer.java
+++ core/src/org/katarine/utils/serialization/Serializer.java
@@ -1,46 +1,12 @@
 package org.katarine.utils.serialization;
 
-import org.katarine.utils.Utils;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public abstract class Serializer<T extends Serializable> {
-    public static <T extends Serializable> ObjectRepresentation<T> serialize(Object obj) {
-        return serialize(obj, obj.getClass());
-    }
-
-    public static <T extends Serializable> ObjectRepresentation<T> serialize(Object obj, Class<?> objClass) {
-//        if (!Serializable.class.isAssignableFrom(objClass)) return null;
-        if (objClass.equals(Object.class)) return null;
-        Class<?> superObjClass = objClass.getSuperclass();
-
-        ObjectRepresentation<? super T> superObjRep = new ObjectRepresentation<>(serialize(superObjClass.cast(obj), superObjClass));
-        for (var f : superObjClass.getDeclaredFields()) {
-            try {
-                superObjRep.addField(f.getName(), Utils.getFieldValue(f, obj));
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        ObjectRepresentation<T> objRep = new ObjectRepresentation<>(superObjRep);
-        for (var f : objClass.getDeclaredFields()) {
-            try {
-                objRep.addField(f.getName(), Utils.getFieldValue(f, obj));
-//                objRep.addFields(((Serializable) obj).getFields());
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        return objRep;
-    }
-
     public File toFile(ObjectRepresentation<T> representation, File file) throws IOException {
-        if (!file.exists()) file.createNewFile();
+        if (!file.exists()) throw new IOException("File does not exists: " + file.getPath());
 
         try(FileOutputStream fos = new FileOutputStream(file)) {
             fos.write(toString(representation).getBytes());
@@ -51,6 +17,30 @@ public abstract class Serializer<T extends Serializable> {
         return file;
     }
 
+    /**
+     * Returns a string representation of the given ObjectRepresentation.
+     *
+     * @param representation the ObjectRepresentation to convert to a string
+     * @return a string representation of the ObjectRepresentation
+     */
     public abstract String toString(ObjectRepresentation<T> representation);
+
+    /**
+     * Returns a string representation of the given ObjectRepresentation.
+     *
+     * @param representation the ObjectRepresentation to convert to a string
+     * @return a string representation of the ObjectRepresentation
+     *
+     * @see {@link Serializer#toString(ObjectRepresentation)}
+     */
+
+    public String serialize(ObjectRepresentation<T> representation) {
+        return toString(representation);
+    }
+
     public abstract ObjectRepresentation<T> fromString(String string);
+
+    public ObjectRepresentation<T> deserialize(String string) {
+        return fromString(string);
+    }
 }
