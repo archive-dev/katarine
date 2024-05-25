c30ec17a7ecef51a8183a140d3d753e64ca21593
whoisamyy <egegegg002@gmail.com>
2024-05-26 00:18:12 +0300

    removed deserialize method

--- core/src/org/katarine/utils/serialization/Serializable.java
+++ core/src/org/katarine/utils/serialization/Serializable.java
@@ -4,8 +4,4 @@ import java.util.HashMap;
 
 public interface Serializable {
     HashMap<String, Object> getFields();
-    Object deserialize(HashMap<String, Object> fields);
-    /*
-     The result of deserialize(getFields()) must return an object which field values are equal to values this object.
-     */
 }
