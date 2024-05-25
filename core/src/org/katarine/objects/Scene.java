fe413da24fa07187f74624e07a87c965cf173812
whoisamyy <egegegg002@gmail.com>
2024-05-26 00:16:33 +0300

    override getFields() method

--- core/src/org/katarine/objects/Scene.java
+++ core/src/org/katarine/objects/Scene.java
@@ -1,11 +1,24 @@
 package org.katarine.objects;
 
 import org.katarine.annotations.EditorObject;
+import org.katarine.utils.serialization.annotations.DontSerialize;
+
+import java.util.HashMap;
 
 @EditorObject
 public class Scene extends GameObject {
+    @DontSerialize
     protected final Scene scene = this;
     public Scene(String name) {
         this.name = name;
+    }
+
+    @Override
+    public HashMap<String, Object> getFields() {
+        final HashMap<String, Object> fields = new HashMap<>();
+        fields.put("scene", this.name);
+//        fields.put("children", this.children);
+
+        return fields;
     }
 }
