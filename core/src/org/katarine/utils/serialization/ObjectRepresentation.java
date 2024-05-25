94192bd81848f103df185f19742671a8ca9f8fe6
whoisamyy <egegegg002@gmail.com>
2024-05-26 00:23:47 +0300

    added Class<?> represented field. moved static methods from Serializer to ObjectRepresentation

--- core/src/org/katarine/utils/serialization/ObjectRepresentation.java
+++ core/src/org/katarine/utils/serialization/ObjectRepresentation.java
@@ -1,12 +1,16 @@
 package org.katarine.utils.serialization;
 
+import org.katarine.utils.Utils;
+
 import java.util.HashMap;
 
 public final class ObjectRepresentation<T extends Serializable> {
     private final HashMap<String, Object> fields = new HashMap<>();
     private final ObjectRepresentation<? super T> superObject;
+    private final Class<?> representedClass;
 
-    public ObjectRepresentation(ObjectRepresentation<? super T> superObject) {
+    public ObjectRepresentation(Class<?> representedClass, ObjectRepresentation<? super T> superObject) {
+        this.representedClass = representedClass;
         this.superObject = superObject;
     }
 
@@ -25,5 +29,49 @@ public final class ObjectRepresentation<T extends Serializable> {
 
     public void addFields(HashMap<String, Object> fields) {
         this.fields.putAll(fields);
+    }
+
+    public Class<?> getRepresentedClass() {
+        return representedClass;
+    }
+
+    public static <T extends Serializable> ObjectRepresentation<T> from(Object obj) {
+        return from(obj, obj.getClass());
+    }
+
+    public static <T extends Serializable> ObjectRepresentation<T> from(Object obj, Class<?> objClass) {
+//        if (!Serializable.class.isAssignableFrom(objClass)) return null;
+        if (objClass.equals(Object.class)) return null;
+        Class<?> superObjClass = objClass.getSuperclass();
+
+        ObjectRepresentation<? super T> superObjRep = new ObjectRepresentation<>(superObjClass, from(obj, superObjClass));
+        for (var f : superObjClass.getDeclaredFields()) {
+            try {
+                String fieldName = f.getName();
+                superObjRep.addField(fieldName, Utils.getFieldValue(f, obj));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        ObjectRepresentation<T> objRep = new ObjectRepresentation<>(objClass, superObjRep);
+        for (var f : objClass.getDeclaredFields()) {
+            try {
+//                System.out.println(f.getName() + "; " + obj.getClass());
+                objRep.addField(f.getName(), Utils.getFieldValue(f, obj));
+//                objRep.addFields(((Serializable) obj).getFields());
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return objRep;
+    }
+
+    public static <T extends Serializable> ObjectRepresentation<T> fromMap(Class<?> clazz, HashMap<String, Object> fieldsMap) {
+        ObjectRepresentation<T> objRep = new ObjectRepresentation<>(clazz, null);
+
+        objRep.addFields(fieldsMap);
+        return objRep;
     }
 }
