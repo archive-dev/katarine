e74db9586bb6dd8b4323457b64397d6ad251db05
whoisamyy <egegegg002@gmail.com>
2024-05-26 00:20:25 +0300

    made Serializable and added default implementation of getFields() method
    added ArrayWrapper class that well wraps an array, so it can be serialized

--- core/src/org/katarine/core/KObject.java
+++ core/src/org/katarine/core/KObject.java
@@ -1,3 +1,62 @@
 package org.katarine.core;
 
-public interface KObject {} // KObject is base class for GameObject, Component and other katarine objects
+import org.katarine.logging.LogLevel;
+import org.katarine.logging.Logger;
+import org.katarine.utils.Utils;
+import org.katarine.utils.serialization.Serializable;
+import org.katarine.utils.serialization.annotations.DontSerialize;
+
+import java.util.HashMap;
+
+// KObject is a base class for GameObject, Component and other katarine objects
+public interface KObject extends Serializable {
+    @Override
+    default HashMap<String, Object> getFields() {
+        try {
+            return getFieldsRecursively(this.getClass(), null, this);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static HashMap<String, Object> getFieldsRecursively(Class<?> clazz, HashMap<String, Object> currentFields, Object obj) throws IllegalAccessException {
+        var l = new Logger(LogLevel.INFO);
+        l.debug(clazz);
+
+        if (currentFields==null) {
+            currentFields = new HashMap<>();
+        }
+
+        if (clazz.isAnnotationPresent(DontSerialize.class)) return getFieldsRecursively(clazz.getSuperclass(), currentFields, obj);
+        for (var f : clazz.getDeclaredFields()) {
+            if (f.isAnnotationPresent(DontSerialize.class)) continue;
+            l.debug(clazz.getSimpleName() + "; " + f.getName());
+            if (f.getType().isArray()) {
+                currentFields.put(f.getName(), new ArrayWrapper<>(((Object[]) Utils.getFieldValue(f, obj))));
+            }
+            else currentFields.put(f.getName(), Utils.getFieldValue(f, obj));
+        }
+        if (clazz.equals(Object.class)) return currentFields;
+        return getFieldsRecursively(clazz.getSuperclass(), currentFields, obj);
+    }
+}
+
+class ArrayWrapper<T> implements Serializable {
+    T[] array;
+    ArrayWrapper(T[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public HashMap<String, Object> getFields() {
+        final HashMap<String, Object> fields = new HashMap<>();
+
+        int c = 0;
+        for (var v : this.array) {
+            fields.put("i"+c, v);
+            c++;
+        }
+
+        return fields;
+    }
+}
