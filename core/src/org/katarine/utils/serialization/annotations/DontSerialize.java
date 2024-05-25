50388d6fcb44b39a39471434be82d9203a50d65e
whoisamyy <egegegg002@gmail.com>
2024-05-26 00:19:28 +0300

    fixed

--- core/src/org/katarine/utils/serialization/annotations/DontSerialize.java
+++ core/src/org/katarine/utils/serialization/annotations/DontSerialize.java
@@ -1,11 +1,12 @@
 package org.katarine.utils.serialization.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({java.lang.annotation.ElementType.FIELD})
-@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DontSerialize {
     String fieldName() default "";
 }
