3b39daaf528e6f628ff1d304cb24539b64064516
whoisamyy <egegegg002@gmail.com>
2024-06-08 20:30:10 +0300

    removed

--- core/src/org/katarine/editor/components/EditorCanvas.java
+++
@@ -1,8 +0,0 @@
-package org.katarine.editor.components;
-
-import org.katarine.ui.Canvas;
-
-public class EditorCanvas extends Canvas {
-    // просто чтобы не детектилось в едиторе
-    // ENG: just to not detect it in editor
-}
