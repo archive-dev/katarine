28b0bfcc7ed33e11bd69dc3fd384347d2880c0f4
whoisamyy <egegegg002@gmail.com>
2024-06-08 22:14:23 +0300

    idk

---
+++ core/src/org/katarine/editor/systems/EditorSystem.java
@@ -0,0 +1,7 @@
+package org.katarine.editor.systems;
+
+import org.katarine.systems.System;
+
+// system that marks current runtime as editor runtime (TO BE CHANGED)
+public class EditorSystem extends System {
+}
