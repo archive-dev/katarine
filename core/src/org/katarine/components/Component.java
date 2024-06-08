f3c84449ab1ff597c24869202d8f11f41c8ab636
whoisamyy <egegegg002@gmail.com>
2024-06-08 21:50:10 +0300

    made fields volatile instead of final because of need to implement multithreading...

--- core/src/org/katarine/components/Component.java
+++ core/src/org/katarine/components/Component.java
@@ -1,38 +1,43 @@
 package org.katarine.components;
 
 import com.badlogic.gdx.math.Vector2;
+import org.katarine.core.KObject;
 import org.katarine.logging.Logger;
+import org.katarine.objects.EntityManager;
 import org.katarine.objects.GameObject;
-import org.katarine.utils.input.AbstractInputHandler;
-import org.katarine.ui.imgui.HideInInspector;
+import org.katarine.systems.SystemManager;
+import org.katarine.editor.imgui.HideInInspector;
 import org.katarine.utils.serialization.annotations.DontSerialize;
 
-import java.util.UUID;
 
-//abstract because there cannot be any empty component
-
-public abstract class Component extends AbstractInputHandler {
-    @HideInInspector
+public abstract class Component implements KObject {
     @DontSerialize
-    public final UUID id = UUID.randomUUID();
+    private volatile boolean initialized = false;
 
     @DontSerialize
-    private boolean initialized = false;
-
-    @DontSerialize
-    private boolean created = false;
+    private volatile boolean created = false;
 
     @HideInInspector
     @DontSerialize
-    public GameObject gameObject;
+    private volatile GameObject gameObject;
 
     @HideInInspector
     @DontSerialize
-    public Transform2D transform;
+    private volatile Transform2D transform;
+
     public int updateOrder = 0; // where 0 is first
+
     @HideInInspector
     @DontSerialize
     protected final Logger logger = new Logger(this.getClass().getTypeName());
+
+    @HideInInspector
+    @DontSerialize
+    private volatile SystemManager systemManager;
+
+    @HideInInspector
+    @DontSerialize
+    private volatile EntityManager entityManager;
 
     public final void init() {
         if (initialized) return;
@@ -42,35 +47,82 @@ public abstract class Component extends AbstractInputHandler {
         initialized = true;
     }
 
-    public final void preAwake() { //временно final
-        if (transform==null) {
-            if (!Transform2D.class.isAssignableFrom(getClass())) {
-                transform = new Transform2D(new Vector2());
-                gameObject.addComponent(transform);
-                gameObject.transform = transform;
-            }
-        }
+    private void preAwake() {
+        if (transform !=null && Transform2D.class.isAssignableFrom(getClass())) return;
+
+        transform = new Transform2D(new Vector2());
+//        gameObject.transform = transform;
     }
 
-    public final void create() {
+    public synchronized final void create() {
         if (!created) {
             start();
             created = true;
         }
     }
-    /**
-     * Called on instantiation of an object. Can be used for example to initialize components.
-     */
-    public void awake() {}
-    protected void start() {}
-    public void update() {}
-    public void die() {} //what to do on dispose
 
-    public final void setGameObject(GameObject gameObject) {
+    public synchronized final void render() {
+        update();
+    }
+
+    public synchronized final void dispose() {
+        die();
+    }
+
+    /**
+     * This method is called during the initialization of the component.
+     * It can be overridden in subclasses
+     * to perform any specific logic or setup required for the component when it is being initialized.
+     */
+    protected void awake() {}
+
+    /**
+     * Called when the component is created for the first time.
+     * This method can be used to perform any initialization logic or setup required for the component.
+     */
+    protected void start() {}
+
+    /**
+     * This method is called on each frame to update the state of the component.
+     * Subclasses can override this method to implement custom update logic for the component.
+     */
+    protected void update() {}
+
+    /**
+     * This method is called when the component needs to be disposed or destroyed.
+     * Subclasses can override this method to implement any custom cleanup logic required for the component.
+     */
+    protected void die() {}
+
+    public final synchronized void setGameObject(GameObject gameObject) {
         this.gameObject = gameObject;
+    }
+
+    public final GameObject getGameObject() {
+        return gameObject;
     }
 
     public final boolean isInitialized() {
         return initialized;
+    }
+
+    public final Transform2D getTransform() {
+        return transform;
+    }
+
+    void setSystemManager(SystemManager systemManager) {
+        this.systemManager = systemManager;
+    }
+
+    final void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public final SystemManager getSystemManager() {
+        return systemManager;
+    }
+
+    public final EntityManager getEntityManager() {
+        return entityManager;
     }
 }
